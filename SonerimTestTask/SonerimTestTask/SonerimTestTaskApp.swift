b30235728c1c02764be99355593067bc81f93fb4
Ivan <yavorin@ukr.net>
2025-02-18 13:01:28 +0200

    Created Xcode project with a couple of ColorAsset's in it

---
+++ SonerimTestTask/SonerimTestTask/SonerimTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonerimTestTaskApp.swift
+//  SonerimTestTask
+//
+//  Created by Ivan_Tests on 18.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SonerimTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
