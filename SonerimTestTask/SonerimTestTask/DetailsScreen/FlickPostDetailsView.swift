f7040c7d01822ecb000697893772d81a69aaa3a7
Ivan <yavorin@ukr.net>
2025-02-25 12:11:17 +0200

    adding just an image without text info fot Compact vertical size class - some iPhones in landscape mode

--- SonerimTestTask/SonerimTestTask/DetailsScreen/FlickPostDetailsView.swift
+++ SonerimTestTask/SonerimTestTask/DetailsScreen/FlickPostDetailsView.swift
@@ -23,17 +23,16 @@ struct FlickPostDetailsView: View {
     
     //MARK: - properties
     //MARK: Handle Main Scroll
-    @State private var naturalOffset: CGFloat = 0
+    @State private var naturalOffset: CGSize = .zero
     @State private var isScrolling:Bool = false
     @State private var imageSize:CGSize = CGSize(width: 1, height: 1)
     @State private var imageOpacity:CGFloat = 1.0
     
     //MARK: Handle Player presentation
     @Environment(PlayerStatus.self) var playerStatus
-    
-//    @State private var playerProgress:CGFloat = 0.1
-    //
-    
+
+    @Environment(\.verticalSizeClass) private var verticalSizeClass
+
     //MARK: -
     init(image: UIImage, title: String,
          postData: some PrintableSortedValuesContainer,
@@ -57,87 +56,95 @@ struct FlickPostDetailsView: View {
     
     var body: some View {
         ZStack(alignment:.bottom) {
-            ScrollView {
-                
+            if case .compact = verticalSizeClass {
                 ImageContainerView(image: image, largeImageURL: largeImageURL)
-                    .background(
-                        GeometryReader(content: { proxy in
-                            let frame = proxy.frame(in: .named("ScrollViewSpace"))
-                            Color.clear
-                                .preference(key: ViewSizeKey.self, value: frame.size)
-                        })
-                    )
-                    .onPreferenceChange(ViewSizeKey.self, perform: {newSize in
-                        imageSize = newSize
-                    })
-                    .offset(y: naturalOffset)
-                    .opacity(imageOpacity)
-                    .zIndex(1) //keep the Image view under the text for it to be scrolled over the Image
-                
-                scrollableContent
-                    .zIndex(1000)//keep the text above the image when scrolled towards the top
-                
+                    .shadow( radius: 10.0)
             }
-            .coordinateSpace(name:"ScrollViewSpace")
-            .onScrollGeometryChange(for: CGFloat.self,
-                                        of: { scrollGeoProxy in
-                scrollGeoProxy.contentOffset.y
-            },
-                                        action: { oldValue, newValue in
-                //keep the image header always at the top
-                naturalOffset = newValue
-                
-                // adjust opacity from 0.7 to 1.0 when the text is scrolled upwards
-                imageOpacity = min(1.0, max(0.7, (imageSize.height - naturalOffset) / imageSize.height))
-            })
-            .onScrollPhaseChange({ oldPhase, newPhase in
-                // Optional:
-                // adjust the bottom buttons horizontal view opacity for scrolling behaviour:
-                
-                if case .interacting = newPhase {
-                    withAnimation(.linear(duration: 0.2)) {
-                        isScrolling = true
-                    }
-                    return
-                }
-                
-                if case .decelerating = newPhase {
-                    withAnimation(.easeIn(duration:0.2)) {
-                        isScrolling = false
-                    }
-                    return
-                }
-                if case .idle = newPhase {
-                    isScrolling = false
-                }
-                
-            })
-            .ignoresSafeArea(.container, edges: .top)
-            
-            
-            //this is needed due to the Apple's new "Observation" framework and data updates monitoring/handling
-            @Bindable var status = playerStatus
-            
-            if playerStatus.isPlayerVisible {
-                
-                
-                PlayerContainerView(title: "Song Title Here",
-                                    details: "Some Long Details for song HERE Some Long Details for song HERE Some Long Details for song HERE Some Long Details for song HERE. ng HERE Some Long Details for song HERE Some Long Details for song HERE",
-                                    playerProgress: $status.playerProgress)
-                .transition(.move(edge: .bottom))
-//                .transition(.asymmetric(insertion: .move(edge: .bottom), removal: .move(edge: .bottom)) )
+            else {
+                scrollableVerticalContent
             }
-            
         }
         .safeAreaInset(edge: .bottom, content: {
             bottomButtonsContainer
         })
     }
     
+    @ViewBuilder private var scrollableVerticalContent: some View {
+        ScrollView {
+            
+            ImageContainerView(image: image, largeImageURL: largeImageURL)
+                .background(
+                    GeometryReader(content: { proxy in
+                        let frame = proxy.frame(in: .named("ScrollViewSpace"))
+                        Color.clear
+                            .preference(key: ViewSizeKey.self, value: frame.size)
+                    })
+                )
+                .onPreferenceChange(ViewSizeKey.self, perform: {newSize in
+                    imageSize = newSize
+                })
+                .offset(naturalOffset)
+                .opacity(imageOpacity)
+                .zIndex(1) //keep the Image view under the text for it to be scrolled over the Image
+            
+            scrollableContent
+                .zIndex(1000)//keep the text above the image when scrolled towards the top
+            
+        }
+        .coordinateSpace(name:"ScrollViewSpace")
+        .onScrollGeometryChange(for: CGFloat.self,
+                                    of: { scrollGeoProxy in
+            scrollGeoProxy.contentOffset.y
+        },
+                                    action: { oldValue, newValue in
+            //keep the image header always at the top
+            naturalOffset = imageOffset(for: newValue)
+            
+            // adjust opacity from 0.7 to 1.0 when the text is scrolled upwards
+            imageOpacity = imageOpacity(forOffset: newValue)
+        })
+        .onScrollPhaseChange({ oldPhase, newPhase in
+            // Optional:
+            // adjust the bottom buttons horizontal view opacity for scrolling behaviour:
+            
+            if case .interacting = newPhase {
+                withAnimation(.linear(duration: 0.2)) {
+                    isScrolling = true
+                }
+                return
+            }
+            
+            if case .decelerating = newPhase {
+                withAnimation(.easeIn(duration:0.2)) {
+                    isScrolling = false
+                }
+                return
+            }
+            if case .idle = newPhase {
+                isScrolling = false
+            }
+            
+        })
+        .ignoresSafeArea(.container, edges: .top)
+        
+        
+        //this is needed due to the Apple's new "Observation" framework and data updates monitoring/handling
+        @Bindable var status = playerStatus
+        
+        if playerStatus.isPlayerVisible {
+            
+            
+            PlayerContainerView(title: "Song Title Here",
+                                details: "Some Long Details for song HERE Some Long Details for song HERE Some Long Details for song HERE Some Long Details for song HERE. ng HERE Some Long Details for song HERE Some Long Details for song HERE",
+                                playerProgress: $status.playerProgress)
+            .transition(.move(edge: .bottom))
+//                .transition(.asymmetric(insertion: .move(edge: .bottom), removal: .move(edge: .bottom)) )
+        }
+    }
+    
     @ViewBuilder
     private var scrollableContent: some View {
         
-        //GeometryReader {_ in
         VStack (alignment: .leading, spacing:16) {
             VStack {
                 Text(title)
@@ -185,6 +192,14 @@ struct FlickPostDetailsView: View {
         .background(.ultraThinMaterial)
         .opacity(isScrolling ? 0.6 : 1.0)
     }
+    
+    private func imageOpacity(forOffset offset:CGFloat) -> CGFloat {
+        return min(1.0, max(0.7, (imageSize.height - naturalOffset.height) / imageSize.height))
+    }
+    
+    private func imageOffset(for value:CGFloat) -> CGSize {
+        return CGSize(width:0, height: value)
+    }
 }
 
 #Preview {
@@ -194,6 +209,7 @@ struct FlickPostDetailsView: View {
                              postData: PostItem.dummy1,
                              mediaURLContainer: PostItem.dummy1,
                              dismissAction: {})
+        .environment(PlayerStatus())
     } else {
         // Fallback on earlier versions
         Text("pre iOs 18 view")
